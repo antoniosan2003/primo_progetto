64b1534baad831a593b5373334c60c301d998121
antoniosan2003 <marasco.tony03@gmail.com>
2022-12-12 10:49:33 +0100

    commit

--- prova_pratica_0/urls.py
+++ prova_pratica_0/urls.py
@@ -4,6 +4,6 @@ from prova_pratica_0.views import index,maxmin,media
 app_name="prova_pratica_0"
 urlpatterns=[
     path('', index, name='index'),
-    path('somma', maxmin, name='somma'),
+    path('maxmin', maxmin, name='maxmin'),
     path('media', media, name='media'),
 ]
